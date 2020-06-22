a17cb4c9c8ec8f999e3b01df1455f5d00ec9833f
BednarJan <63300374+BednarJan@users.noreply.github.com>
2020-06-22 14:23:18 +0200

    Added Voetsh VC4060 and Weiss VK1000

---
+++ TC/CTC_VOETSCH_VC4060.vb
@@ -0,0 +1,110 @@
+﻿Imports Ivi.Visa
+Imports System.Math
+
+Public Class CTC_VOETSCH_VC4060
+    Inherits BCTC_WEISS
+    Implements ITC
+
+#Region "Shorthand Properties"
+
+
+#End Region
+
+#Region "Constructor"
+    Public Sub New(Session As IMessageBasedSession, ErrorLogger As CErrorLogger)
+
+        MyBase.New(Session, ErrorLogger)
+
+        Name = "VOETSCH VC4060"
+
+        MinTemp = -75
+        MaxTemp = 130
+
+        MinHumidity = Single.MinValue
+        MaxHumidity = Single.MaxValue
+
+        MinPressure = Single.MinValue
+        MaxPressure = Single.MaxValue
+
+    End Sub
+#End Region
+
+#Region "Basic Device Functions (IDevice)"
+
+    Overrides Sub Initialize() Implements IDevice.Initialize
+
+        SetpointTemp = 25
+        SetpointHumidity = 0
+        SetpointPressure = Single.MinValue
+
+        HeatGrad = 3  ' K/min
+        CoolGrad = 3  ' K/min
+        HumiGrad = 2.5 ' K/min
+        DeHumiGrad = 2.5 ' K/min
+
+        SetGradients()
+
+        TurnOFF()
+
+    End Sub
+#End Region
+
+#Region "Interface ITC Methodes"
+
+
+
+#End Region
+
+#Region "private methodes and functions"
+
+    Overrides Sub SetTempAndHumidity(ByVal temp As Single, Optional hum As Single = 0, Optional bONOff As Boolean = True)
+
+        MyBase.SetTempAndHumidity(temp, hum, bONOff)
+
+    End Sub
+
+
+    Overrides Function GetTempAndHumidity(ByRef hum As Single) As Single
+
+        Dim Buffer As String
+        Dim temp As Single = Single.MinValue
+
+        Dim errMsg As String = vbNullString
+        Dim sHlp() As String
+
+
+        For iii As Integer = 1 To 5
+
+            ' TC returns "-075.0 -038.3 0101000000000000Z"
+
+            Visa.SendString("$00I")
+            cHelper.Delay(1)
+            Buffer = Visa.ReceiveString(errMsg)
+            sHlp = Split(Buffer, " ")
+
+            If IsNumeric(sHlp(1)) Then
+                temp = Convert.ToSingle(sHlp(1))
+                Exit For
+            End If
+        Next iii
+
+        hum = Single.MinValue
+
+        Return temp
+
+    End Function
+
+
+    Overrides Sub SetGradients()
+
+    End Sub
+
+#End Region
+
+
+End Class
+
+
+
+
+
---
+++ TC/CTC_WEISS_VK1000.vb
@@ -0,0 +1,87 @@
+﻿Imports Ivi.Visa
+Imports System.Math
+
+Public Class CTC_WEISS_VK1000
+
+    Inherits BCTC_WEISS
+    Implements ITC
+
+#Region "Shorthand Properties"
+
+#End Region
+
+#Region "Constructor"
+    Public Sub New(Session As IMessageBasedSession, ErrorLogger As CErrorLogger)
+
+        MyBase.New(Session, ErrorLogger)
+
+        Name = "Weiss WK1000"
+        MinTemp = -75
+        MaxTemp = 130
+
+        MinHumidity = 0
+        MaxHumidity = 100
+
+        MinPressure = Single.MinValue
+        MaxPressure = Single.MaxValue
+
+    End Sub
+#End Region
+
+#Region "Basic Device Functions (IDevice)"
+
+    Overrides Sub Initialize() Implements IDevice.Initialize
+
+        SetpointTemp = 25
+        SetpointHumidity = 0
+        SetpointPressure = Single.MinValue
+
+        HeatGrad = 3  ' K/min
+        CoolGrad = 3  ' K/min
+        HumiGrad = 2.5 ' K/min
+        DeHumiGrad = 2.5 ' K/min
+
+        SetGradients()
+
+        TurnOFF()
+
+    End Sub
+#End Region
+
+#Region "Interface ITC Methodes"
+
+
+
+#End Region
+
+#Region "private methodes and functions"
+
+    Overrides Sub SetTempAndHumidity(ByVal temp As Single, Optional hum As Single = 0, Optional bONOff As Boolean = True)
+
+        MyBase.SetTempAndHumidity(temp, hum, bONOff)
+
+    End Sub
+
+
+    Overrides Function GetTempAndHumidity(ByRef hum As Single) As Single
+
+        Return MyBase.GetTempAndHumidity(hum)
+
+    End Function
+
+
+    Overrides Sub SetGradients()
+
+        MyBase.SetGradients()
+
+    End Sub
+
+#End Region
+
+
+End Class
+
+
+
+
+
